e851535b9aaa551539bb43f050abc419a4281a2c
Grant Goldman <grant.goldman@ucsf.edu>
2022-01-11 11:41:16 -0800

    add search w/o path

--- search/graph.py
+++ search/graph.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from typing import Dict, List
 
 class Graph:
     """
@@ -7,8 +8,8 @@ class Graph:
     def __init__(self, filename: str):
         """
         Initialization of graph object which serves as a container for 
-        methods to load data and 
-        
+        methods to load data and
+
         """
         self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
 
@@ -19,9 +20,37 @@ class Graph:
         * If there's no end node, just return a list with the order of traversal
         * If there is an end node and a path exists, return a list of the shortest path
         * If there is an end node and a path does not exist, return None
-
+        
         """
-        return
+
+        visited = [start]
+        q = [start]
+        child_parent = {start: None}
+
+        while q:
+            curr = q.pop(0)
+
+            for out_n in self.graph.neighbors(curr):
+                if out_n not in visited:
+                    q.append(out_n)
+                    visited.append(out_n)
+                    if end:
+                        child_parent[out_n] = curr
+
+        if not end:
+            return visited
+        else:
+
+            if end in child_parent:
+                return []
+            return None
+
+    def unravel_dict(d: Dict) -> List:
+        """
+        
+        """
+
+
 
 
 
