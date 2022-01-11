4b671ff03e8634fdcc7991076161bdf472405a0c
Grant Goldman <grant.goldman@ucsf.edu>
2022-01-11 13:13:41 -0800

    remove fixture from tests?

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -2,13 +2,12 @@
 import pytest
 from search import graph
 
-@pytest.fixture
 def test_bfs_traversal():
     """
     TODO: Write your unit test for a breadth-first
-    traversal here. Create an instance of your Graph class 
-    using the 'tiny_network.adjlist' file and assert 
-    that all nodes are being traversed (ie. returns 
+    traversal here. Create an instance of your Graph class
+    using the 'tiny_network.adjlist' file and assert
+    that all nodes are being traversed (ie. returns
     the right number of nodes, in the right order, etc.)
     """
     g = graph.Graph("./data/tiny_network.adjlist")
@@ -32,14 +31,14 @@ def test_bfs_traversal():
 
 def test_bfs():
     """
-    TODO: Write your unit test for your breadth-first 
+    TODO: Write your unit test for your breadth-first
     search here. You should generate an instance of a Graph
-    class using the 'citation_network.adjlist' file 
-    and assert that nodes that are connected return 
+    class using the 'citation_network.adjlist' file
+    and assert that nodes that are connected return
     a (shortest) path between them.
-    
-    Include an additional test for nodes that are not connected 
-    which should return None. 
+
+    Include an additional test for nodes that are not connected
+    which should return None.
     """
     g = graph.Graph("./data/citation_network.adjlist")
 
